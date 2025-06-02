8d91c880a2691ff9665178bd3e87c74299cc36ee
Shifra Destiny Ssebuliba <destinyssebuliba@gmail.com>
2025-06-02 23:10:16 +0200

    Updated calculator's logic to use a different approach

--- src/index.js
+++ src/index.js
@@ -1,22 +1,54 @@
-const displayBox = document.querySelector("#input-row-box");
+let newLine = true;
+let currentOperator;
+let value1;
 
-// appendToDisplay() functionality
-function appendToDisplay(input) {
-  displayBox.value += input;
-}
-
-// clearDisplay() functionality
-function clearDisplay() {
-  displayBox.value = "";
-}
-
-// calculate() functionality
-function calculate() {
-  try {
-    displayBox.value = eval(displayBox.value);
-  } catch (error) {
-    displayBox.value = "Error";
+// Handling for when a digit is pressed
+function digitButton(button) {
+  if (newLine) {
+    document.getElementById("input-row-box").value = button;
+    newLine = false;
+  } else {
+    let currentValue = document.getElementById("input-row-box").value;
+    document.getElementById("input-row-box").value = currentValue + button;
   }
+}
+
+// Handling for AC button
+function clearButton() {
+  document.getElementById("input-row-box").value = "";
+  newLine = true;
+}
+
+// Handling for operator buttons
+function operatorButton(operator) {
+  currentOperator = operator;
+  value1 = parseInt(document.getElementById("input-row-box").value);
+  newLine = true;
+}
+
+// Handling for operations
+function equalButton() {
+  let value2 = parseInt(document.getElementById("input-row-box").value);
+  let finalTotal;
+
+  switch (currentOperator) {
+    case "+":
+      finalTotal = value1 + value2;
+      break;
+    case "-":
+      finalTotal = value1 - value2;
+      break;
+    case "*":
+      finalTotal = value1 * value2;
+      break;
+    case "/":
+      finalTotal = value1 / value2;
+      break;
+  }
+
+  document.getElementById("input-row-box").value = finalTotal;
+  value1 = 0;
+  newLine = true;
 }
 
 // Dark theme functionality
